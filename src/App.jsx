729fc89b3bc029d7e4f66c3628eae7cfcf46bfb5
matikasssssss <matias.farias@ug.uchile.cl>
2026-09-18 02:25:54 -0300

    feat: fetch done

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,32 @@
 import './App.css'
 import { Movies } from './components/Movies'
 import { useMovies } from './hooks/useMovies'
-import { useState } from 'react'
-
+import { useSearch } from './hooks/useSearch'
 
 function App() {
-  const { movies: mappedMovies } = useMovies()
-  const [query, setQuery] = useState('')
+  const { search, updateSearch, error } = useSearch()
+  const { movies: mappedMovies, getMovies } = useMovies( {search} )
 
   const handleSubmit = (event) => {
     event.preventDefault()
-    console.log('Searching for:', query)
+    getMovies()
   }
 
   const handleChange = (event) => {
-    setQuery(event.target.value)
+    updateSearch(event.target.value)
   }
 
   return (
     <div className="page">
       <header>
-        <h1> Movie Browser </h1>
+        <h1> Buscador de peliculas </h1>
         <form className="form" onSubmit={handleSubmit}>
-          <input value={query} onChange={handleChange} name="query" placeholder="Search..." />
+          <input value={search} onChange={handleChange} name="query" placeholder="Search..." />
           <button type="submit">Search</button>
         </form>
+        <div 
+          className="error">{error && <p style={{ color: 'red' }}>{error}</p>}
+        </div>
       </header>
 
       <main>
